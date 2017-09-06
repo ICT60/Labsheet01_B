6684c6ec1801d8340b39c390577f3954166ff21b
Chatchai Saratakij <CSaratakij@gmail.com>
2017-09-07 01:23:41 +0700

    Print text from loaded template.

---
+++ Labsheet01_B/Labsheet01_B/AsciiArt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Labsheet01_B
+{
+    class AsciiArt
+    {
+        List<string> _loadedTemplateText;
+
+
+        public AsciiArt()
+        {
+            _loadedTemplateText = new List<string>();
+        }
+
+        public bool LoadTemplate(string templatePath)
+        {
+            var isSuccessful = File.Exists(templatePath);
+
+            if (isSuccessful)
+            {
+                _loadedTemplateText.Clear();
+
+                try
+                {
+                    using (StreamReader reader = new StreamReader(templatePath))
+                    {
+                        var text = reader.ReadLine();
+
+                        do
+                        {
+                            _loadedTemplateText.Add(text);
+                            text = reader.ReadLine();
+
+                        } while (text != null);
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    isSuccessful = false;
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            return isSuccessful;
+        }
+
+        public void Show()
+        {
+            foreach (string text in _loadedTemplateText)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ Labsheet01_B/Labsheet01_B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+
+namespace Labsheet01_B
+{
+    class Program
+    {
+        const string TEMPLATE_FILE_NAME = "template.txt";
+
+
+        static void Main(string[] args)
+        {
+            var templateDir = Directory.GetCurrentDirectory();
+            templateDir += ("\\" + TEMPLATE_FILE_NAME);
+
+            var asciiArt = new AsciiArt();
+
+            if (File.Exists(templateDir))
+            {
+                asciiArt.LoadTemplate(TEMPLATE_FILE_NAME);
+                asciiArt.Show();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
